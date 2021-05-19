ee5036073f0730fd61ac3c7b1191d6954c7bb004
Doruk Ornekci <dornekci@gmail.com>
2021-05-19 15:59:40 +0300

    Classes added.

--- src/main.py
+++ src/main.py
@@ -1,40 +1,79 @@
-def Edge(src, dst):
+class Edge:
+
+    # The initialization function for the edge class
+    def __init__(self, src, dst):
+        self.src = src
+        self.dst = dst
 
 
-    """Buraya inputları storelasak yeter
+class Node:
 
-       Inputların get funcs"""
+    # The initialization function for the node class
+    def __init__(self, id, kingdom, neighbors, isInKingdom):
+        self.id = id
+        self.kingdom = kingdom
+        self.neighbors = neighbors
+        self.isInKingdom = isInKingdom
 
-def Node(num, kingdom, neighbor, isInKingdom):
+    # Function for adding a neighbor to neighbor list
+    def add_neighbor(self, neighbor):
 
-    """ Initialize Node func
+        if neighbor in self.neighbors:
+            print("Neighbor is already in neighbor list")
 
-     change Kingdom func
+        else:
+            self.neighbors.append(neighbor)
 
-     add / remove neighbor funcs
+    # Function for removing a neighbor from the neighbor list
+    def remove_neighbor(self, neighbor):
 
-     isInKindgom change func
+        if neighbor in self.neighbors:
+            self.neighbors.remove(neighbor)
 
-     inputların get funcs"""
-
-
-def Graph():
-    nodes = {}
-    edges = {}
-
-""" Add / remove node funcs
-
-     Add / remove edge funcs
-
-     Get nodes /get edges
-
-     Visualize graph func"""
+        else:
+            print("Neighbor is not in the neighbor list")
 
 
+class Graph:
+    nodes = []
+    edges = []
 
-def readFromTxt(path):
+    def add_node(self, node):
 
-    """" Tüm txt file i okuyup graph returnleyecek"""
+        if node in self.nodes:
+            print("Node is already in the node list")
+
+        else:
+            self.nodes.append(node)
+
+    def remove_node(self, node):
+
+        if node in self.nodes:
+            self.nodes.remove(node)
+
+        else:
+            print("Node is not in node list")
+
+    def add_edge(self, edge):
+
+        if edge in self.edge:
+            print("Edge is already in the edge list")
+
+        else:
+            self.edges.append(edge)
+
+    def remove_edge(self, edge):
+
+        if edge in self.edges:
+            self.edges.remove(edge)
+
+        else:
+            print("Edge is not in edge list")
 
 
-#if __name__ == '__main__':
+def read_txt(path):
+    return 0
+
+
+if __name__ == '__main__':
+    print(0)
